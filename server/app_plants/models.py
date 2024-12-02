8a0353bb9484f1e500bbc687ec033e2922e2082a
nurfianqodar <77nurfianqodar@gmail.com>
2024-12-02 12:18:27 +0700

    get plant endpoint

---
+++ server/app_plants/models.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+
+
+class Classification(BaseModel):
+    kingdom: str
+    division: str
+    plant_class: str = Field(serialization_alias="class")
+    order: str
+    family: str
+    genus: str
+    speciess: str
+
+
+class PlantName(BaseModel):
+    scientific: str
+    local: str
+    national: str
+
+
+class Plant(BaseModel):
+    description: str
+    name: PlantName
+    classification: Classification
