e6d12475dc2366adeafd485287223b398bfc242b
nurfianqodar <77nurfianqodar@gmail.com>
2024-12-13 15:35:49 +0700

    fix responses

--- server/lib_utils/tensorflow.py
+++ server/lib_utils/tensorflow.py
@@ -7,6 +7,7 @@ import numpy as np
 # Load the saved model
 rice_model = keras.models.load_model("models/rice_model.h5")
 potato_model = keras.models.load_model("models/potato_model.h5")
+tomato_model = keras.models.load_model("models/tomato_model.h5")
 
 
 def predict_rice_image(image):
@@ -34,17 +35,19 @@ def predict_rice_image(image):
         1: "xanthomonas_oryzae",
         2: "ustilaginoidea_virens",
         3: None,
-        4: "health",
+        4: "healthy",
     }  # Example
 
     predicted_label = label_map.get(predicted_class, None)
 
     if predicted_label is not None or predicted_label is not "health":
-        return {"label": predicted_label, "confidence": float(confidence)}
+        return {
+            "plant_slug": "oryza_sativa",
+            "label": predicted_label,
+            "confidence": float(confidence),
+        }
     elif predicted_label is None:
         return {"error": "this image is not leaf"}
-    elif predicted_label is "health":
-        return {"label": predicted_label, "confidence": float(confidence)}
 
 
 def predict_potato_image(image):
@@ -70,15 +73,55 @@ def predict_potato_image(image):
     label_map = {
         0: "alternaria_solani",
         1: "phytophthora_infestans",
-        2: "health",
+        2: "healthy",
         3: None,
     }  # Example
 
     predicted_label = label_map.get(predicted_class, None)
 
     if predicted_label is not None or predicted_label is not "health":
-        return {"label": predicted_label, "confidence": float(confidence)}
+        return {
+            "plant_slug": "solanum_tuberosum",
+            "label": predicted_label,
+            "confidence": float(confidence),
+        }
     elif predicted_label is None:
         return {"error": "this image is not leaf"}
-    elif predicted_label is "health":
-        return {"label": predicted_label, "confidence": float(confidence)}
+
+
+def predict_tomato_image(image):
+    """
+    Predicts the class of an image using the loaded model.
+    """
+    img = tf.keras.preprocessing.image.load_img(image, target_size=(416, 416))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array /= 255.0  # Normalize
+
+    predictions = tomato_model.predict(img_array)
+    predicted_class = np.argmax(predictions[0])
+    confidence = predictions[0][predicted_class]
+
+    # Assuming you have a label map (replace with your actual label map)
+    label_map = {
+        0: "septoria",
+        1: "tomato_yellow_leaf_curl_virus",
+        2: "healthy",
+        3: "liriomyza_huidobrensis",
+        4: "phytophthora_infestans",
+        5: "tomato_mosaic_virus",
+        6: "cladosporium_fulvum",
+        7: "alternaria_solani",
+        8: "tetranychus_spp",
+    }
+
+    predicted_label = label_map.get(predicted_class, None)
+
+    if predicted_label is not None or predicted_label is not "health":
+        return {
+            "plant_slug": "solanum_lycopersicum",
+            "label": predicted_label,
+            "confidence": float(confidence),
+        }
+    elif predicted_label is None:
+        return {"error": "this image is not leaf"}
